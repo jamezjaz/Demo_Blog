162fb3604e162a07f1e7e3e67bb378d3847b9cfb
Jamezjaz <jamezjaz@gmail.com>
2021-06-20 17:14:58 +0100

    docs: Creates comments create action
    docs: Adds comments count on post show page

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_20_154846) do
+ActiveRecord::Schema.define(version: 2021_06_20_155823) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 2021_06_20_154846) do
     t.text "comment"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "post_id"
   end
 
   create_table "posts", force: :cascade do |t|
